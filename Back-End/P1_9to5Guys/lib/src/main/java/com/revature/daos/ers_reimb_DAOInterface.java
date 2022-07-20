cdab457c65c5836497daa7c9fdcd5c1ba08029fd
Ahmad Khalif <awkwardfellowz@gmail.com>
2022-07-20 18:51:02 -0400

    ReimbursementDAOInterface.

--- Back-End/P1_9to5Guys/lib/src/main/java/com/revature/daos/ers_reimb_DAOInterface.java
+++ Back-End/P1_9to5Guys/lib/src/main/java/com/revature/daos/ers_reimb_DAOInterface.java
@@ -1,5 +1,25 @@
 package com.revature.daos;
 
+import java.util.ArrayList;
+
+import com.revature.models.ers_reimbursement;
+
 public interface ers_reimb_DAOInterface {
 
+	// employee functions--------------------------------------------------------------
+	// create a new reimbursement
+	public boolean submitReimb(ers_reimbursement reimbursemet);
+	
+	public ArrayList<ers_reimbursement> viewAllReimbursementsbyUser(int reimb_author);
+	
+	
+	// managerial functions -----------------------------------------------------------
+	// view all reimbursements as a manager...
+	public ArrayList<ers_reimbursement> viewAllReimbursements();
+	
+	// view all reimbursements filtered by a type
+	public ArrayList<ers_reimbursement> viewAllReimbursementsbyType(int reimb_type_id);
+
+	public boolean resolveRequest(int reimb_id, int reimb_status_id);
+	
 }
