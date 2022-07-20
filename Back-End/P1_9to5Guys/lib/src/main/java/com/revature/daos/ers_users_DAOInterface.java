502d1d594d176de5578bf0922abfa9d062d86476
Ahmad Khalif <awkwardfellowz@gmail.com>
2022-07-20 19:05:23 -0400

    DAOInterfaces filled with methods. ConnectionUtil

--- Back-End/P1_9to5Guys/lib/src/main/java/com/revature/daos/ers_users_DAOInterface.java
+++ Back-End/P1_9to5Guys/lib/src/main/java/com/revature/daos/ers_users_DAOInterface.java
@@ -1,5 +1,14 @@
 package com.revature.daos;
 
+// import com.revature.models.ers_users; 
+// add above if base P1 completed
+
 public interface ers_users_DAOInterface {
+	
+	public boolean login(String user_name, String password);
+	
+	// we only want to make a new employee user as
+	// ... as this method isn't really necessary for base project
+	// public boolean newEmployeeUser(ers_users user);
 
 }
