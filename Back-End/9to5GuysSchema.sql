4b6822ed02e90f1b0463ed503bf8f9937c6cc65e
Ahmad Khalif <awkwardfellowz@gmail.com>
2022-07-23 19:35:27 -0400

    the joy of sql: join query test 1

--- Back-End/9to5GuysSchema.sql
+++ Back-End/9to5GuysSchema.sql
@@ -38,6 +38,19 @@ CREATE TABLE ers_users (
 	user_role_id SMALLINT REFERENCES ers_user_roles (ers_user_role_id)
 );
 
+INSERT INTO ers_users (username, PASSWORD, first_name, last_name, user_email, user_role_id)
+VALUES ('sputnik', 'supercool', 'Randy', 'Clocker', 'rcgrimes@gmail.com', 1),
+	   ('moonman', 'control', 'Gavin', 'Puts', 'gambastra@gmail.com', 1),
+	   ('readyFreddy', 'plzdonthack', 'Freddy', 'Kruegar', 'havenofear@gmail.com', 2),
+	   ('Bredleees', 'yo', 'Bradly', 'Case', 'enoughofthat@gmail.com', 2),
+	   ('yoJams', 'musicheals', 'Lamont', 'Porter', 'soundslikehome@gmail.com', 2);
+
+INSERT INTO ers_reimbursement(reimb_amount, reimb_submitted, reimb_resolved, reimb_description, reimb_author, reimb_resolver, reimb_status_id, reimb_type_id)
+VALUES (1000, now(), NULL, 'i need some milk', 4, NULL, 1, 3),
+	   (100, now(), NULL, 'pocono''s infiltration', 5, NULL, 1, 1),
+	   (2000, now(), NULL, 'no cap I just want money lol', 5, NULL, 1, 4),
+	   (600, now(), NULL, 'serious business', 3, NULL, 1, 2);
+
 -- the actual reimbursement requests
 CREATE TABLE ers_reimbursement (
 	reimb_id serial PRIMARY KEY,
@@ -52,7 +65,20 @@ CREATE TABLE ers_reimbursement (
 );
 
 
+SELECT * FROM ers_reimbursement JOIN ers_users ON ers_reimbursement.reimb_author = ers_users.user_id;
 
+SELECT * FROM ers_reimbursement;
+
+SELECT * FROM ers_users;
+
+SELECT * FROM ers_reimbursement_status;
+
+SELECT * FROM ers_reimbursement_type;
+
+SELECT * FROM ers_reimbursement AS er 
+FULL JOIN ers_users AS us ON er.reimb_author = us.user_id AND er.reimb_resolver  = us.user_id 
+FULL JOIN ers_reimbursement_status AS stat ON er.reimb_status_id = stat.reimbursement_status_id 
+FULL JOIN ers_reimbursement_type AS ty ON er.reimb_type_id = ty.reimbursement_type_id;
 
 
 
