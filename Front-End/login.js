55608cb3219bb7ae663de16bd39aea7d0ff331ec
Ahmad Khalif <awkwardfellowz@gmail.com>
2022-07-27 23:36:40 -0400

    login front end functional. I found the cookie! :)

--- Front-End/login.js
+++ Front-End/login.js
@@ -1,24 +1,57 @@
-// base url
-const api = "localhost:/4000/"
+const url = "http://localhost:4000"; // base url
 
 // set event listeners
-document.getElementById("btn").onClick = loginFunc;
+// document.getElementById("viewPassBtn").onClick = viewPassFunc;
+document.getElementById("log").onclick = loginFunc // button to send user's login credentials
 
+
+// send user's login credentials as JSON to the backend
 async function loginFunc(){
 
     // user input into var
     let usernameAttempt = document.getElementById("username").value
     let passwordAttempt = document.getElementById("password").value
 
-    //use the fetch method to send a request to the login API with the user's input
-    let response = await fetch(api + ) 
+    // to send this login data as JSON we first need to create a JS object
+    let userCreds = {
+        username: usernameAttempt,
+        password: passwordAttempt
+    } // left: JSON.  Right: values being stored 
 
-    // check promise
-    if(response.status === 200) {
+    // for debugging, print out userCreds
+    console.log(userCreds)
+
+    // fetch request to server
+    let response = await fetch(url + "/login", {
+        method: "POST",
+        body: JSON.stringify(userCreds),
+        credentials: "include"
+        // this line will ensure a cookie gets captured (so that we can use sessions)
+        // future fetches after login will require this "include" variable***************************************
+
+    })
+    
+    // log status
+    console.log(response.status)
+
+    //
+    if (response.status === 202){
+
+        let data = await response.json();
+        console.log(data)
+
+        // this is where to redirect the user
 
     } else {
-        document.
+
+        // the header will change if
+        document.getElementById("informalHead").innerHTML = "Login failed. Please check your username and password.";
+        document.getElementById("informalHead").style.color = "red";
+
     }
-
-
 }
+
+
+
+// ben recommends having one login page.
+// based on the user's user_role_id reroute to appropriate page 
